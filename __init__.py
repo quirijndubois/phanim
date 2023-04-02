90e29bca0a9c1a54be4f5457812d97d335c7f239
quirijndaboyy <69307964+quirijndaboyy@users.noreply.github.com>
2023-04-02 15:22:01 +0200

    Delete __init__.py

--- __init__.py
+++
@@ -1,8 +0,0 @@
-import phanim.info as info
-from phanim import intro
-intro.printIntro()
-
-from phanim.window import Window
-from phanim.physics import *
-import phanim.fourier_optics as optics
-# from phanim.quickplot import *
